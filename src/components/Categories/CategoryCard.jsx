61c690904a341f047cacd4b7653abc2b77110d15
minhkq309 <quangminhkim3009@gmail.com>
2025-08-02 08:12:22 +0700

    Update

--- src/components/Categories/CategoryCard.jsx
+++ src/components/Categories/CategoryCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export default function CategoryCard({ icon: Icon, title, subtitle }) {
+export default function CategoryCard({ icon, title, subtitle }) {
   return (
     <div className="group rounded-xl bg-white p-5 flex items-center gap-4 hover:shadow-primary transition-all duration-200 cursor-pointer w-full">
       {/* Icon container */}
       <div className="w-16 h-16 md:w-18 md:h-18 lg:w-[72px] lg:h-[72px] rounded-lg flex items-center justify-center bg-primary-50 text-primary-500 transition-all duration-200 group-hover:bg-primary-500 group-hover:text-white">
-        <Icon className="w-10 h-10 transition-all duration-200" />
+        {React.createElement(icon, { className: "w-10 h-10 transition-all duration-200" })}
       </div>
 
       {/* Text content */}
